4d3f8e2a735e9f4c3538296b64794be98d0c16e7
Schuchardt <schuchardt@softed.de>
2019-07-08 09:06:14 +0200

    Testing improved

--- test/function-Start-Log.tests.ps1
+++ test/function-Start-Log.tests.ps1
@@ -3,14 +3,14 @@ Remove-Module $moduleName -Force -ErrorAction SilentlyContinue
 
 Import-Module "$PSScriptRoot\..\$moduleName.psd1"
 
-# Mock functions
-function TestFile ([String]$Path) {
-    $file = 1 | Select-Object -Property @{Name = "LastWriteTime";Expression = {[datetime]::Now}},
-                                        @{Name = "FullName";Expression = {$Path}}
-    return $file
-}
-
  InModuleScope -ModuleName $moduleName {
+    # Mock functions
+    function TestFile ([String]$Path) {
+        $file = 1 | Select-Object -Property @{Name = "LastWriteTime";Expression = {[datetime]::Now}},
+                                            @{Name = "FullName";Expression = {$Path}}
+        return $file
+    }
+
     Describe "Start-Log Function Test" {
         Mock -CommandName New-Item -MockWith {
             TestFile -Path "C:\Temp\new.txt"
